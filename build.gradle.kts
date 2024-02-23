7f9d1ce8301729fb741f6f701d2b62d58926330d
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-23 21:11:33 +0300

    ok

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+plugins {
+	id("org.jetbrains.kotlin.jvm") version "1.9.22"
+	id("application")
+}
+
+group = "com.github.hummel"
+version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
+
+val embed: Configuration by configurations.creating
+
+dependencies {
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+	embed("com.google.code.gson:gson:2.10.1")
+	embed("org.apache.httpcomponents.client5:httpclient5:5.3")
+	embed("com.formdev:flatlaf:3.2.5")
+	embed("com.formdev:flatlaf-intellij-themes:3.2.5")
+	implementation("com.google.code.gson:gson:2.10.1")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.3")
+	implementation("com.formdev:flatlaf:3.2.5")
+	implementation("com.formdev:flatlaf-intellij-themes:3.2.5")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(8)
+	}
+}
+
+application {
+	mainClass = "com.github.hummel.cfmu.MainKt"
+}
+
+tasks {
+	named<JavaExec>("run") {
+		standardInput = System.`in`
+	}
+	jar {
+		manifest {
+			attributes(
+				mapOf(
+					"Main-Class" to "com.github.hummel.cfmu.MainKt"
+				)
+			)
+		}
+		from(embed.map {
+			if (it.isDirectory) it else zipTree(it)
+		})
+		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+	}
+}
---
+++ src/main/kotlin/com/github/hummel/cfmu/Main.kt
@@ -0,0 +1,160 @@
+package com.github.hummel.cfmu
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.classic.methods.HttpPost
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.core5.http.ContentType
+import org.apache.hc.core5.http.io.entity.EntityUtils
+import java.io.File
+
+fun main() {
+	print("Enter token: ")
+
+	val token = readln()
+
+	val idToVersion = getMappingsFromAPI(token)
+
+	val exampleFileName = "[1.3.2] API Upload Test 24.02.29 (Forge).jar"
+
+	val projectIds = arrayOf(
+		389886, //The Rings of Power
+		654414, //Legendary Item
+		663217 //Dirt Tools and Armor
+	)
+
+	projectIds.forEach { project ->
+		publishProject(project, idToVersion, token)
+	}
+}
+
+private fun publishProject(
+	project: Int, idToVersion: MutableMap<Int, String>, token: String
+) {
+	val files = File("folders/$project").listFiles() ?: return
+	val fileComparator = Comparator<File> { file1, file2 ->
+		val version1 = file1.name.extractMcVersion()
+		val version2 = file2.name.extractMcVersion()
+
+		val parts1 = version1.split('.').map { it.toInt() }
+		val parts2 = version2.split('.').map { it.toInt() }
+
+		return@Comparator (0 until 3).asSequence().map {
+			parts1[it].compareTo(parts2[it])
+		}.firstOrNull {
+			it != 0
+		} ?: 0
+	}
+
+	files.sortWith(fileComparator)
+
+	files.forEach { jar ->
+		val (mcVersionId, modLoaderId) = getJarInfo(jar, idToVersion)
+		val serverSidenessId = 9639
+		val clientSidenessId = 9638
+
+		HttpClients.createDefault().use {
+			val url = "https://minecraft.curseforge.com/api/projects/$project/upload-file"
+
+			val request = HttpPost(url)
+			request.addHeader("X-Api-Token", token)
+
+			val multipartEntityBuilder = MultipartEntityBuilder.create()
+			multipartEntityBuilder.addTextBody(
+				"metadata", """
+										{
+											"changelog": "",
+											"gameVersions": [$mcVersionId, $modLoaderId, $serverSidenessId, $clientSidenessId],
+											"releaseType": "release"
+										}
+										""".trimIndent(), ContentType.APPLICATION_JSON
+			)
+			multipartEntityBuilder.addBinaryBody(
+				"file", jar, ContentType.DEFAULT_BINARY, jar.name
+			)
+
+			request.entity = multipartEntityBuilder.build()
+
+			val response = it.execute(request) { response ->
+				val entity = response.entity
+				EntityUtils.toString(entity)
+			}
+
+			println(response)
+		}
+	}
+}
+
+private val modLoaderMappings = mapOf(
+	"NeoForge" to 10150, //NeoForge
+	"Forge" to 7498, //Forge
+	"Fabric" to 7499, //Fabric
+	"Quilt" to 9153 //Quilt
+)
+
+private fun getJarInfo(
+	jar: File, idToVersion: MutableMap<Int, String>
+): Pair<Int, Int> {
+	val mcVersion = jar.name.extractMcVersion()
+	val modLoader = jar.name.extractModLoader()
+
+	var mcVersionId = 0
+
+	for ((id, value) in idToVersion) {
+		if (value == mcVersion) {
+			mcVersionId = id
+			break
+		}
+	}
+
+	require(mcVersionId != 0)
+
+	return mcVersionId to (modLoaderMappings[modLoader] ?: error("Wrong ModLoader ID!"))
+}
+
+private fun getMappingsFromAPI(token: String): MutableMap<Int, String> {
+	HttpClients.createDefault().use {
+		val request = HttpGet("https://minecraft.curseforge.com/api/game/versions")
+		request.addHeader("X-Api-Token", token)
+
+		val response = it.execute(request) { response ->
+			val entity = response.entity
+
+			EntityUtils.toString(entity)
+		}
+
+		return@getMappingsFromAPI parseMappings(response)
+	}
+}
+
+private fun parseMappings(json: String): MutableMap<Int, String> {
+	val gson = Gson()
+
+	val listType = object : TypeToken<List<GameVersion>>() {}.type
+	val gameVersions = gson.fromJson<List<GameVersion>>(json, listType)
+
+	val idToVer = mutableMapOf<Int, String>()
+	gameVersions.forEach {
+		idToVer[it.id] = it.name
+	}
+
+	return idToVer
+}
+
+private fun String.extractMcVersion(): String {
+	val versionRegex = Regex("""\[(.+)\]""")
+	val versionMatch = versionRegex.find(this)
+	return versionMatch?.groupValues?.get(1) ?: ""
+}
+
+private fun String.extractModLoader(): String {
+	val versionRegex = Regex("""\((.+)\)""")
+	val versionMatch = versionRegex.find(this)
+	return versionMatch?.groupValues?.get(1) ?: ""
+}
+
+data class GameVersion(
+	val id: Int, val gameVersionTypeID: Int, val name: String, val slug: String, val apiVersion: String?
+)
