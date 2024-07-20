29aa26a9d9d41447ae836e46234b0069d1bb1472
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-20 12:54:30 +0300

    1

--- appModrinth/build.gradle.kts
+++ appModrinth/build.gradle.kts
@@ -13,7 +13,7 @@ val embed: Configuration by configurations.creating
 
 dependencies {
 	embed(project(":appCommon"))
-	
+
 	embed("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
 	embed("com.google.code.gson:gson:2.11.0")
 	embed("org.apache.httpcomponents.client5:httpclient5:5.3.1")
